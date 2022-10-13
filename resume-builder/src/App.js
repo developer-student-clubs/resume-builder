155f2f3713fb7824f275ada5a31a5e9b379ec3e9
patelyash5208 <patelyash5208@gmail.com>
2022-10-13 21:19:20 +0530

    footer

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <HomePage />
+      <Footer />
     </>
   );
 }
